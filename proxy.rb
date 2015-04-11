a7e0ccec06bcb9156d2444736b637385133b0088
Kris <elhowm@gmail.com>
2015-04-12 00:05:05 +0300

    fix proxy

--- proxy.rb
+++ proxy.rb
@@ -3,89 +3,89 @@
 class Student
   attr_accessor :author_info
 
-  def initialize info
+  def initialize(info)
     @author_info = info
   end
 end
 
 class StudentPR < Student
   def make_little_program
-    p 'Welcome to the little program creator'
+    puts 'Welcome to the little program creator'
     sleep 1
-    p 'Start working..'
-    p 'Huyak..'
+    puts 'Start working..'
+    puts 'Huyak..'
     sleep 2
-    p 'Ta-da! Little program successfully created!'
-    p 'Author Info:'
-    p @author_info
+    puts 'Ta-da! Little program successfully created!'
+    puts 'Author Info:'
+    puts @author_info
   end
 
   def make_middle_program
-    p 'Welcome to the middle program creator'
+    puts 'Welcome to the middle program creator'
     sleep 1
-    p 'Start working..'
+    puts 'Start working..'
     2.times do
-      p 'Huyak..'
+      puts 'Huyak..'
       sleep 2
     end
-    p 'Ta-da! Middle program successfully created!'
-    p 'Author Info:'
-    p @author_info
+    puts 'Ta-da! Middle program successfully created!'
+    puts 'Author Info:'
+    puts @author_info
   end
 
   def make_huge_program
-    p 'Welcome to the huge program creator'
+    puts 'Welcome to the huge program creator'
     sleep 1
-    p 'Start working..'
+    puts 'Start working..'
     3.times do
-      p 'Huyak..'
+      puts 'Huyak..'
       sleep 2
     end
-    p 'Ta-da! Huge program successfully created!'
-    p 'Author Info:'
-    p @author_info
+    puts 'Ta-da! Huge program successfully created!'
+    puts 'Author Info:'
+    puts @author_info
   end
 end
 
 class StudentKS < Student
   def make_little_board
-    p 'Welcome to the little digital board creator'
+    puts 'Welcome to the little digital board creator'
     sleep 1
-    p 'Start working..'
+    puts 'Start working..'
     sleep 1
-    p 'Huyak..'
+    puts 'Huyak..'
     sleep 2
-    p 'Ta-da! Little digital board successfully created!'
-    p 'Author Info:'
-    p @author_info
+    puts 'Ta-da! Little digital board successfully created!'
+    puts 'Author Info:'
+    puts @author_info
   end
 
   def make_middle_board
-    p 'Welcome to the middle digital board creator'
+    puts 'Welcome to the middle digital board creator'
     sleep 1
-    p 'Start working..'
+    puts 'Start working..'
     sleep 1
     2.times do
-      p 'Huyak..'
+      puts 'Huyak..'
       sleep 2
     end
-    p 'Ta-da! Middle digital board successfully created!'
-    p 'Author Info:'
-    p @author_info
+    puts 'Ta-da! Middle digital board successfully created!'
+    puts 'Author Info:'
+    puts @author_info
   end
 
   def make_huge_board
-    p 'Welcome to the huge digital board creator'
+    puts 'Welcome to the huge digital board creator'
     sleep 1
-    p 'Start working..'
+    puts 'Start working..'
     sleep 1
     3.times do
-      p 'Huyak..'
+      puts 'Huyak..'
       sleep 2
     end
-    p 'Ta-da! Huge digital board successfully created'
-    p 'Author Info:'
-    p @author_info
+    puts 'Ta-da! Huge digital board successfully created'
+    puts 'Author Info:'
+    puts @author_info
   end
 end
 
@@ -123,6 +123,6 @@ end
 college = College.new
 
 college.make_little_board
-p '---------------------------------------------------'
+puts '---------------------------------------------------'
 sleep 1
 college.make_huge_program
